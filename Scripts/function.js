8385aeb6fe4a10bd761a55a583d9c62981e09d71
DenisTsirlin <denis.tsirlin@gmail.com>
2023-05-19 17:45:29 +0300

    שינויים בהכל
    צריך רק את לפתור את העניין שיהיה לפחות רווח של משבצת אחת בין ספינה לספינה

--- Scripts/function.js
+++ Scripts/function.js
@@ -1,34 +1,111 @@
-
-
-function createTable() {
-    /* These lines of code are selecting the HTML elements with the IDs "form2" and "table-container"
-    respectively and assigning them to the variables `form` and `tableContainer`. This allows the
-    JavaScript code to interact with these elements and manipulate their contents. */
-    /* This code is creating a table based on the user's input and displaying it on the webpage. */
-    let form = document.querySelector('#form2');
-    const tableContainer = document.querySelector('#table-container');
-    form.addEventListener('submit', function (event) {
-        const selectedSize = document.querySelector('input[name="gender"]:checked').value;
-        const numRows = selectedSize;
-        const numCols = selectedSize;
-        let table = "<table>";
-        for (let i = 0; i < numRows; i++) {
-            table += "<tr>";
-            for (let j = 0; j < numCols; j++) {
-                table += "<td style='background-color: gray'></td>";
-            }
-            table += "</tr>";
+function generateBoard() {
+    var previousTable = document.querySelector('table');
+    if (previousTable) {
+      previousTable.parentNode.removeChild(previousTable);
+    }
+  
+    var boardSize = parseInt(document.getElementById('board-size').value);
+    var board = [];
+    var table = document.createElement('table');
+    for (var i = 0; i < boardSize; i++) {
+      var row = document.createElement('tr');
+      var rowData = [];
+      for (var j = 0; j < boardSize; j++) {
+        var cell = document.createElement('td');
+        cell.classList.add('empty');
+        cell.addEventListener('click', handleCellClick);
+        row.appendChild(cell);
+        rowData.push(cell);
+      }
+      table.appendChild(row);
+      board.push(rowData);
+    }
+    document.body.appendChild(table);
+  }
+  
+  function placeShips() {
+    var boardSize = parseInt(document.getElementById('board-size').value);
+    var shipCounts = {
+      2: parseInt(document.getElementById('ship-2').value),
+      3: parseInt(document.getElementById('ship-3').value),
+      4: parseInt(document.getElementById('ship-4').value),
+      5: parseInt(document.getElementById('ship-5').value)
+    };
+  
+    var board = document.getElementsByTagName('td');
+    for (var size = 5; size >= 2; size--) {
+      for (var count = 0; count < shipCounts[size]; count++) {
+        var orientation = Math.random() < 0.5 ? 'horizontal' : 'vertical';
+        var shipPlaced = false;
+        while (!shipPlaced) {
+          var startCell = getRandomCell();
+          if (canPlaceShip(startCell, size, orientation, boardSize)) {
+            placeShip(startCell, size, orientation, boardSize);
+            shipPlaced = true;
+          }
         }
-        table += "</table>";
+      }
+    }
+  }
+  
+  function getRandomCell() {
+    var board = document.getElementsByTagName('td');
+    var randomIndex = Math.floor(Math.random() * board.length);
+    return board[randomIndex];
+  }
+  
+  function canPlaceShip(startCell, size, orientation, boardSize) {
+    var board = document.getElementsByTagName('td');
+    var startIndex = Array.prototype.indexOf.call(board, startCell);
+    var endIndex;
+  
+    if (orientation === 'horizontal') {
+      endIndex = startIndex + size - 1;
+      if (endIndex % boardSize < startIndex % boardSize) {
+        // Ship goes beyond the board boundary
+        return false;
+      }
+    } else {
+      endIndex = startIndex + (size - 1) * boardSize;
+      if (endIndex >= board.length) {
+        // Ship goes beyond the board boundary
+        return false;
+      }
+    }
+    for (var i = startIndex; i <= endIndex; i++) {
+    if (board[i].classList.contains('occupied')) {
+      // Overlapping with another ship
+      return false;
+    }
+  }
 
-
-        tableContainer.innerHTML = table;
-        /* `event.preventDefault();` is preventing the default behavior of the form submission. In this
-        case, it is preventing the page from reloading when the form is submitted. */
-        event.preventDefault();
-    });
-
-
+  return true;
 }
 
+function placeShip(startCell, size, orientation, boardSize) {
+  var board = document.getElementsByTagName('td');
+  var startIndex = Array.prototype.indexOf.call(board, startCell);
+  var endIndex;
 
+  if (orientation === 'horizontal') {
+    endIndex = startIndex + size - 1;
+    for (var i = startIndex; i <= endIndex; i++) {
+      board[i].classList.add('occupied');
+    }
+  } else {
+    endIndex = startIndex + (size - 1) * boardSize;
+    for (var i = startIndex; i <= endIndex; i += boardSize) {
+      board[i].classList.add('occupied');
+    }
+  }
+}
+
+function handleCellClick(event) {
+  var cell = event.target;
+  if (cell.classList.contains('occupied')) {
+    cell.classList.remove('occupied');
+    cell.classList.add('hit');
+  } else {
+    cell.classList.add('miss');
+  }
+}
--- Scripts/index.js
+++ Scripts/index.js
@@ -1,8 +1,20 @@
 
-function Menu() {
-   let size=document.getElementById('#create');
-   size=document.addEventListener('click',createTable)
-
-}
-
-Menu();
+ function Manu() {
+   var boardSize = parseInt(document.getElementById('board-size').value);
+   var shipCounts = {
+     2: parseInt(document.getElementById('ship-2').value),
+     3: parseInt(document.getElementById('ship-3').value),
+     4: parseInt(document.getElementById('ship-4').value),
+     5: parseInt(document.getElementById('ship-5').value)
+   };
+ 
+   var totalShips = Object.values(shipCounts).reduce((total, count) => total + count, 0);
+   var allShipsValid = Object.values(shipCounts).every(count => count >= 1);
+ 
+   if (boardSize && totalShips > 0 && allShipsValid) {
+     generateBoard();
+     placeShips();
+   } else {
+     alert('Please provide all the required information and ensure all ships have a minimum inventory of 1, and choose which size you want to play.');
+   }
+ }
