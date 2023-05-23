11e1f4c2035b9fb016b4263ee6274a0256505e55
DenisTsirlin <denis.tsirlin@gmail.com>
2023-05-23 19:20:31 +0300

    new update

--- Scripts/function.js
+++ Scripts/function.js
@@ -1,97 +1,115 @@
 function generateGameTable(boardSize) {
     var gameTable = document.getElementById('game-table');
     gameTable.innerHTML = '';
-    gameTable.classList.add('gray-board'); // Add gray-board class
-  
+    gameTable.classList.add('gray-board');
+
     for (var i = 0; i < boardSize; i++) {
-      var row = document.createElement('tr');
-      for (var j = 0; j < boardSize; j++) {
-        var cell = document.createElement('td');
-        row.appendChild(cell);
-      }
-      gameTable.appendChild(row);
+        var row = document.createElement('tr');
+
+        for (var j = 0; j < boardSize; j++) {
+            var cell = document.createElement('td');
+            row.appendChild(cell);
+        }
+
+        gameTable.appendChild(row);
     }
-  }
-  
-  function generateBattleships(boardSize, ship2Count, ship3Count, ship4Count, ship5Count) {
+}
+
+function generateBattleships(boardSize, ship2Count, ship3Count, ship4Count, ship5Count) {
     var slots = document.querySelectorAll('#game-table td');
     slots.forEach(function (slot) {
-      slot.classList.remove('battleship', 'clicked');
-      slot.style.backgroundColor = '';
+        slot.classList.remove('battleship', 'clicked');
+        slot.style.backgroundColor = '';
     });
-  
+
     var battleshipCount = ship2Count + ship3Count + ship4Count + ship5Count;
     var battleshipLengths = [];
+
     for (var i = 0; i < ship2Count; i++) {
-      battleshipLengths.push(2);
+        battleshipLengths.push(2);
     }
+
     for (var i = 0; i < ship3Count; i++) {
-      battleshipLengths.push(3);
+        battleshipLengths.push(3);
     }
+
     for (var i = 0; i < ship4Count; i++) {
-      battleshipLengths.push(4);
+        battleshipLengths.push(4);
     }
+
     for (var i = 0; i < ship5Count; i++) {
-      battleshipLengths.push(5);
+        battleshipLengths.push(5);
     }
+
     var directions = ['horizontal', 'vertical'];
 
-  battleshipLengths.forEach(function (length) {
-    var direction = directions[Math.floor(Math.random() * directions.length)];
-    var isPlaced = false;
+    battleshipLengths.forEach(function (length) {
+        var direction = directions[Math.floor(Math.random() * directions.length)];
+        var isPlaced = false;
 
-    while (!isPlaced) {
-      var randomIndex = Math.floor(Math.random() * slots.length);
-      var startSlot = slots[randomIndex];
-      var startRowIndex = Math.floor(randomIndex / boardSize);
-      var startColIndex = randomIndex % boardSize;
+        while (!isPlaced) {
+            var randomIndex = Math.floor(Math.random() * slots.length);
+            var startSlot = slots[randomIndex];
+            var startRowIndex = Math.floor(randomIndex / boardSize);
+            var startColIndex = randomIndex % boardSize;
 
-      if (direction === 'horizontal') {
-        if (startColIndex + length <= boardSize) {
-          var validPlacement = true;
-          for (var i = 0; i < length; i++) {
-            var slotIndex = randomIndex + i;
-            if (slots[slotIndex].classList.contains('battleship') ||
-              (startRowIndex > 0 && slots[slotIndex - boardSize].classList.contains('battleship')) ||
-              (startRowIndex < boardSize - 1 && slots[slotIndex + boardSize].classList.contains('battleship')) ||
-              (startColIndex > 0 && slots[slotIndex - 1].classList.contains('battleship')) ||
-              (startColIndex + length < boardSize && slots[slotIndex + 1].classList.contains('battleship'))) {
-              validPlacement = false;
-              break;
+            if (direction === 'horizontal') {
+                if (startColIndex + length <= boardSize) {
+                    var validPlacement = true;
+
+                    for (var i = 0; i < length; i++) {
+                        var slotIndex = randomIndex + i;
+
+                        if (
+                            slots[slotIndex].classList.contains('battleship') ||
+                            (startRowIndex > 0 && slots[slotIndex - boardSize].classList.contains('battleship')) ||
+                            (startRowIndex < boardSize - 1 && slots[slotIndex + boardSize].classList.contains('battleship')) ||
+                            (startColIndex > 0 && slots[slotIndex - 1].classList.contains('battleship')) ||
+                            (startColIndex + length < boardSize && slots[slotIndex + 1].classList.contains('battleship'))
+                        ) {
+                            validPlacement = false;
+                            break;
+                        }
+                    }
+
+                    if (validPlacement) {
+                        for (var i = 0; i < length; i++) {
+                            var slotIndex = randomIndex + i;
+                            slots[slotIndex].classList.add('battleship');
+                        }
+
+                        isPlaced = true;
+                    }
+                }
+            } else if (direction === 'vertical') {
+                if (startRowIndex + length <= boardSize) {
+                    var validPlacement = true;
+
+                    for (var i = 0; i < length; i++) {
+                        var slotIndex = randomIndex + i * boardSize;
+
+                        if (
+                            slots[slotIndex].classList.contains('battleship') ||
+                            (startRowIndex > 0 && slots[slotIndex - boardSize].classList.contains('battleship')) ||
+                            (startRowIndex + length < boardSize && slots[slotIndex + boardSize].classList.contains('battleship')) ||
+                            (startColIndex > 0 && slots[slotIndex - 1].classList.contains('battleship')) ||
+                            (startColIndex < boardSize - 1 && slots[slotIndex + 1].classList.contains('battleship'))
+                        ) {
+                            validPlacement = false;
+                            break;
+                        }
+                    }
+
+                    if (validPlacement) {
+                        for (var i = 0; i < length; i++) {
+                            var slotIndex = randomIndex + i * boardSize;
+                            slots[slotIndex].classList.add('battleship');
+                        }
+
+                        isPlaced = true;
+                    }
+                }
             }
-          }
-          if (validPlacement) {
-            for (var i = 0; i < length; i++) {
-              var slotIndex = randomIndex + i;
-              slots[slotIndex].classList.add('battleship');
-            }
-            isPlaced = true;
-          }
         }
-      } else if (direction === 'vertical') {
-        if (startRowIndex + length <= boardSize) {
-          var validPlacement = true;
-          for (var i = 0; i < length; i++) {
-            var slotIndex = randomIndex + i * boardSize;
-            if (slots[slotIndex].classList.contains('battleship') ||
-              (startRowIndex > 0 && slots[slotIndex - boardSize].classList.contains('battleship')) ||
-              (startRowIndex + length < boardSize && slots[slotIndex + boardSize].classList.contains('battleship')) ||
-              (startColIndex > 0 && slots[slotIndex - 1].classList.contains('battleship')) ||
-              (startColIndex < boardSize - 1 && slots[slotIndex + 1].classList.contains('battleship'))) {
-              validPlacement = false;
-              break;
-            }
-          }
-
-          if (validPlacement) {
-            for (var i = 0; i < length; i++) {
-              var slotIndex = randomIndex + i * boardSize;
-              slots[slotIndex].classList.add('battleship');
-            }
-            isPlaced = true;
-          }
-        }
-      }
-    }
-  });
+    });
 }
--- Scripts/index.js
+++ Scripts/index.js
@@ -1,20 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-   var gameForm = document.getElementById('game-form');
-   gameForm.addEventListener('submit', startGame);
- 
-   var gameTable = document.getElementById('game-table');
-   gameTable.addEventListener('click', handleSlotClick);
- });
- 
- function startGame(event) {
-   event.preventDefault();
- 
-   var boardSize = parseInt(document.getElementById('board-size').value);
-   var ship2Count = parseInt(document.getElementById('ship-2').value);
-   var ship3Count = parseInt(document.getElementById('ship-3').value);
-   var ship4Count = parseInt(document.getElementById('ship-4').value);
-   var ship5Count = parseInt(document.getElementById('ship-5').value);
-   if (
+function Main() {
+  document.addEventListener('DOMContentLoaded', function () {
+    var gameForm = document.getElementById('game-form');
+    gameForm.addEventListener('submit', startGame);
+
+    var gameTable = document.getElementById('game-table');
+    gameTable.addEventListener('click', handleSlotClick);
+  });
+
+  function startGame(event) {
+    event.preventDefault();
+
+    var boardSize = parseInt(document.getElementById('board-size').value);
+    var ship2Count = parseInt(document.getElementById('ship-2').value);
+    var ship3Count = parseInt(document.getElementById('ship-3').value);
+    var ship4Count = parseInt(document.getElementById('ship-4').value);
+    var ship5Count = parseInt(document.getElementById('ship-5').value);
+    if (
       isNaN(boardSize) ||
       isNaN(ship2Count) ||
       isNaN(ship3Count) ||
@@ -24,29 +25,52 @@ document.addEventListener('DOMContentLoaded', function () {
       alert('Please select all the required options');
       return;
     }
-  
+
     generateGameTable(boardSize);
     generateBattleships(boardSize, ship2Count, ship3Count, ship4Count, ship5Count);
+    var scoreCells = document.querySelectorAll('.score-table table td:nth-child(2)');
+    scoreCells[0].textContent = ship2Count.toString();
+    scoreCells[1].textContent = ship3Count.toString();
+    scoreCells[2].textContent = ship4Count.toString();
+    scoreCells[3].textContent = ship5Count.toString();
   }
-  
+
   function handleSlotClick(event) {
     var slot = event.target;
+
     if (slot.classList.contains('battleship')) {
-      slot.style.backgroundColor = 'orange';
-      updateScore(slot);
-    } else if (!slot.classList.contains('clicked')) {
+      if (slot.style.backgroundColor !== 'orange') {
+        slot.style.backgroundColor = 'orange';
+
+        var shipSize = parseInt(slot.getAttribute('data-ship-size'));
+        var shipId = 'score-ship-' + shipSize;
+        var scoreCell = document.getElementById(shipId);
+
+        if (scoreCell) {
+          var currentScore = parseInt(scoreCell.textContent);
+
+          if (currentScore > 0) {
+            currentScore--;
+            scoreCell.textContent = currentScore.toString();
+
+            if (currentScore === 0) {
+              // All slots of the ship have been hit
+              scoreCell.parentNode.classList.add('score-zero');
+
+              // Check if all ships have been sunk
+              var allShipsSunk = checkAllShipsSunk();
+              displayBoomMessage();
+            }
+          }
+        }
+      }
+    }
+    else if (!slot.classList.contains('clicked')) {
       slot.style.backgroundColor = 'blue';
-    slot.classList.add('clicked');
-  } else if (slot.classList.contains('clicked')) {
-    slot.style.backgroundColor = '';
-    slot.classList.remove('clicked');
+      slot.classList.add('clicked');
+    }
   }
 }
 
-function updateScore(slot) {
-   var shipLength = slot.classList.length - 1; // Subtract 1 to exclude the 'battleship' class
-   var shipId = 'ship-' + shipLength;
-   var scoreCell = document.querySelector('.table1 .' + shipId + ' + td');
-   var currentScore = parseInt(scoreCell.textContent);
-   scoreCell.textContent = currentScore + 1;
- }
+
+Main();
