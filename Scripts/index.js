47efad5d150bbdb91ceceaec62282c9fa67b6e5b
dorprati <dorprati@gmail.com>
2023-05-28 09:22:13 +0300

    UI change + working ship recognition + working score table

--- Scripts/index.js
+++ Scripts/index.js
@@ -2,97 +2,174 @@ function Main() {
   document.addEventListener('DOMContentLoaded', function () {
     var gameForm = document.getElementById('game-form');
     gameForm.addEventListener('submit', startGame);
-
-    var gameTable = document.getElementById('game-table');
-    gameTable.addEventListener('click', handleSlotClick);
   });
+}
 
-  function startGame(event) {
-    event.preventDefault();
-    var boardSize = parseInt(document.getElementById('board-size').value);
-    var ship2Count = parseInt(document.getElementById('ship-2').value);
-    var ship3Count = parseInt(document.getElementById('ship-3').value);
-    var ship4Count = parseInt(document.getElementById('ship-4').value);
-    var ship5Count = parseInt(document.getElementById('ship-5').value);
-    if (
-      isNaN(boardSize) ||
-      isNaN(ship2Count) ||
-      isNaN(ship3Count) ||
-      isNaN(ship4Count) ||
-      isNaN(ship5Count)
-    ) {
-      alert('Please select all the required options');
-      return;
-    }
-    showTable();
-    generateGameTable(boardSize);
-    generateBattleships(boardSize, ship2Count, ship3Count, ship4Count, ship5Count);
-    var scoreCells = document.querySelectorAll('.score-table table td:nth-child(2)');
-    scoreCells[0].textContent = ship2Count.toString();
-    scoreCells[1].textContent = ship3Count.toString();
-    scoreCells[2].textContent = ship4Count.toString();
-    scoreCells[3].textContent = ship5Count.toString();
+function startGame(event) {
+  event.preventDefault();
+  var boardSize = parseInt(document.getElementById('board-size').value);
+  var ship2Count = parseInt(document.getElementById('ship-2').value);
+  var ship3Count = parseInt(document.getElementById('ship-3').value);
+  var ship4Count = parseInt(document.getElementById('ship-4').value);
+  var ship5Count = parseInt(document.getElementById('ship-5').value);
+  if (
+    isNaN(boardSize) ||
+    isNaN(ship2Count) ||
+    isNaN(ship3Count) ||
+    isNaN(ship4Count) ||
+    isNaN(ship5Count)
+  ) {
+    alert('Please select all the required options');
+    return;
   }
-  function handleSlotClick(event) {
-    var slot = event.target;
+  showTable();
+  generateGameTable(boardSize);
+  generateBattleships(boardSize, ship2Count, ship3Count, ship4Count, ship5Count);
+}
 
-    if (slot.classList.contains('battleship')) {
-      if (slot.style.backgroundColor !== 'orange') {
-        slot.style.backgroundColor = 'orange';
-/* `var shipSize = parseInt(slot.getAttribute('data-ship-size'));` is getting the value of the
-`data-ship-size` attribute of the clicked slot and converting it to an integer using `parseInt()`.
-This value is used to determine the size of the battleship that was hit, which is then used to
-update the score and check if the ship has been sunk. */
-        var shipSize = parseInt(slot.getAttribute('data-value'));
-        var shipId = 'score-ship-' + shipSize;
-        var scoreCell = document.getElementById(shipId);
-          battleShipHit();
-          shipSize--;
-        if(shipSize===0)
-        {
-          battleShipDown(); 
-        }
-      
-        if (scoreCell) {
-          var currentScore = parseInt(scoreCell.textContent);
-  
-          if (currentScore > 0) {
-            // Reduce the score by one 
-            scoreCell.textContent = (currentScore - 1).toString();
-  
-            if (currentScore === 1) {
-              // All slots of the ship have been hit
-              scoreCell.parentNode.classList.add('score-zero');
-  
-              // Handle the ship being sunk
-              handleShipSunk(shipSize);
+function showTable() {
+  var gameTable = document.getElementById('game-container');
+  gameTable.style.display = 'flex';
+  gameTable.style.flexWrap = 'wrap';
+  gameTable.innerHTML = '';
+}
 
-              // Check if all ships have been sunk
-              var allShipsSunk = checkAllShipsSunk();
-              if (allShipsSunk) {
-                // Game over, display a message or perform any other actions
-                console.log('All ships sunk. Game over!');
-
-                // Add animation and sound here
-                slot.classList.add('enlarged');
-                playSound();
-
-                // Remove the 'enlarged' class after the animation has completed
-                slot.addEventListener('animationend', function() {
-                  slot.classList.remove('enlarged');
-                });
-              }
-            }
-          }
-        }
-      }
-    } else if (!slot.classList.contains('clicked')) {
-      slot.style.backgroundColor = 'blue';
-      slot.classList.add('clicked');
-    }
+function generateGameTable(boardSize) {
+  var gameContainer = document.getElementById('game-container');
+  gameContainer.style.setProperty('--board-size', boardSize);
+  var slotCount = boardSize * boardSize;
+  for (var i = 0; i < slotCount; i++) {
+    var slot = document.createElement('div');
+    slot.className = 'slot';
+    slot.addEventListener('click', handleSlotClick);
+    gameContainer.appendChild(slot);
   }
 }
 
+function generateBattleships(boardSize, ship2Count, ship3Count, ship4Count, ship5Count) {
+  var slots = document.getElementsByClassName('slot');
+  var shipSizes = [];
 
+  for (var i = 0; i < ship2Count; i++) {
+    shipSizes.push(2);
+  }
+  for (var i = 0; i < ship3Count; i++) {
+    shipSizes.push(3);
+  }
+  for (var i = 0; i < ship4Count; i++) {
+    shipSizes.push(4);
+  }
+  for (var i = 0; i < ship5Count; i++) {
+    shipSizes.push(5);
+  }
+
+  for (var i = 0; i < shipSizes.length; i++) {
+    var shipSize = shipSizes[i];
+    var randomIndex = Math.floor(Math.random() * slots.length);
+    var shipSlots = [];
+
+    if (randomIndex + shipSize <= slots.length) {
+      for (var j = 0; j < shipSize; j++) {
+        shipSlots.push(slots[randomIndex + j]);
+      }
+    } else {
+      for (var j = 0; j < shipSize; j++) {
+        shipSlots.push(slots[randomIndex - j]);
+      }
+    }
+
+    shipSlots.forEach(function (slot) {
+      slot.classList.add('battleship');
+      slot.setAttribute('data-ship-size', shipSize);
+    });
+  }
+}
+
+function handleSlotClick(event) {
+  var slot = event.target;
+
+  if (slot.classList.contains('battleship')) {
+    if (!slot.classList.contains('hit')) {
+      slot.classList.add('hit');
+      slot.style.backgroundColor = 'red';
+      var shipSize = parseInt(slot.getAttribute('data-ship-size'));
+      updateScore(shipSize);
+      checkShipSunk(shipSize);
+      var gameOver = checkAllShipsSunk(ship2Count, ship3Count, ship4Count, ship5Count);
+      if(gameOver)
+      {
+        alert('Game Over');
+        restartGame()
+      }
+    }
+  } else if (!slot.classList.contains('clicked')) {
+    slot.classList.add('clicked');
+    slot.style.backgroundColor = 'gray';
+  }
+}
+
+function updateScore(shipSize) {
+  var scoreCell = document.getElementById('score-' + shipSize);
+  var score = parseInt(scoreCell.textContent);
+  scoreCell.textContent = score + 1;
+}
+
+function checkShipSunk(shipSize) {
+  var scoreCell = document.getElementById('score-' + shipSize);
+  var sunkCell = document.getElementById('sunk-' + shipSize);
+  var score = parseInt(scoreCell.textContent);
+
+  if (score === shipSize) {
+    sunkCell.textContent = parseInt(sunkCell.textContent) + 1;
+    scoreCell.textContent = 0;
+    alert('Ship of size ' + shipSize + ' has been sunk!');
+  }
+}
+
+function updateScoreTable(ship2Count, ship3Count, ship4Count, ship5Count) {
+  var scoreCells = document.querySelectorAll('.score-table table td:nth-child(2)');
+  var sunkCells = document.querySelectorAll('.score-table table td:nth-child(3)');
+  scoreCells[0].textContent = '0';
+  scoreCells[1].textContent = '0';
+  scoreCells[2].textContent = '0';
+  scoreCells[3].textContent = '0';
+  sunkCells[0].textContent = '0';
+  sunkCells[1].textContent = '0';
+  sunkCells[2].textContent = '0';
+  sunkCells[3].textContent = '0';
+}
+function checkAllShipsSunk(ship2Count, ship3Count, ship4Count, ship5Count) {
+  var sumAllShips = parseInt(ship2Count + ship3Count + ship4Count + ship5Count);
+  var sunkCells = document.querySelectorAll('.score-table table td:nth-child(3)');
+  for (var i = 0; i < sunkCells.length; i++) {
+    var currentScore = parseInt(sunkCells[i].textContent);
+    alert(currentScore)
+    alert(sumAllShips)
+    if (currentScore != sumAllShips) {
+      // At least one ship is not sunk
+      return false;
+    }
+    if(currentScore === sumAllShips)
+    {
+        // All ships are sunk
+  
+  return true;
+    }
+
+  }
+}
+
+function restartGame() {
+  // Clear the game board
+  var gameTable = document.getElementById('game-table');
+  gameTable.innerHTML = '';
+
+    // Reset the score table
+    var scoreCells = document.querySelectorAll('.score-table table td:nth-child(2)');
+    for (var i = 0; i < scoreCells.length; i++) {
+      scoreCells[i].textContent = '0';
+      scoreCells[i].parentNode.classList.remove('score-zero');
+    }
+}
 
 Main();
